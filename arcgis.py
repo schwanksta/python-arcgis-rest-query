64b3bc3d745b6b6245fe10fbf65ffbdc5741737f
Ken Schwencke <schwank@gmail.com>
2014-11-11 15:38:18 -0500

    Removed KML cruft and started in on translating the Arc JSON format, which is a lot more reliable and less overhead. so far points and multipoints work

--- arcgis.py
+++ arcgis.py
@@ -1,11 +1,5 @@
 import json
 import requests
-from zipfile import ZipFile
-from cStringIO import StringIO
-from osgeo import ogr
-import tempfile
-from BeautifulSoup import BeautifulSoup
-
 
 def urljoin(*args):
     """
@@ -35,6 +29,11 @@ class ArcGIS:
         self.base_url=base_url
         self.folder=folder
         self.map=map_name
+        self._layer_descriptor_cache = {}
+        self._geom_parsers = {
+            'esriGeometryPoint': self._parse_esri_point,
+            'esriGeometryMultipoint': self._parse_esri_multipoint
+        }
 
     def _build_request(self, layer):
         return urljoin(self.base_url,
@@ -48,54 +47,66 @@ class ArcGIS:
     def _build_query_request(self, layer):
         return urljoin(self._build_request(layer), "query")
 
-    def get_kml(self, layer, where="1 = 1", fields=[], return_geometry=True):
+    def get_json(self, layer, where="1 = 1", fields=[], srid='4326'):
         """
-        Gets the KMZ file from ArcGIS, unzips it in memory and returns the 
-        contents of the KML file inside.
+        Gets the JSON file from ArcGIS
         """
-        return ZipFile(StringIO(requests.get(self._build_query_request(layer),
+        return requests.get(self._build_query_request(layer),
             params = {
                 'where': where,
                 'outFields': ", ".join(fields),
-                'returnGeometry': return_geometry,
-                'f': "kmz"
-            }).content), "r").open('doc.kml', 'r').read()
+                'returnGeometry': True,
+                'outSR': srid,
+                'f': "pjson",
+                'orderByFields': "OBJECTID"
+            }).json()
 
-    def _table_to_properties(self, obj):
-        """
-        Takes the HTML description table in the JSON and 
-        parses it into a real object we can replace the 
-        HTML-infested version with.
-        """
-        html = obj.get("properties").get("Description")
-        
-        if html == '':
-            # Sometimes there is no description HTML, so we pass
-            # the existing properties object back out.
-            return obj.get("properties")
+    def _parse_esri_point(self, geom):
+        return {
+            "type": "Point",
+            "coordinates": [
+                geom.get('x'),
+                geom.get('y')
+            ]
+        }
+    def _parse_esri_multipoint(self, geom):
+        return {
+            "type": "MultiPoint",
+            "coordinates": geom.get('points')
+        }
 
-        soup = BeautifulSoup(html)
-        # For some reason sometimes there's a nested table, and that has the info.
-        tables = soup.findAll("table")
-        if len(tables) > 1:
-            table = tables[1]
-        else:
-            table = tables[0]
-        tds = map(lambda x: x.getText(), table.findAll("td"))
-        ret = {}
-        # Split up the tds into pairs and zip em together as dicts.
-        for pair in zip(tds[::2], tds[1::2]):
-            ret.update({pair[0]: pair[1]})
-        return ret
+    def _parse_esri_polyline(self, geom):
+        pass
+    def _parse_esri_polygon(self, geom):
+        pass
+
+    def _determine_geom_parser(self, type):
+        return self._geom_parsers.get(type)
+
+    def esri_to_geojson(self, obj, geom_parser):
+        return {
+            "type": "Feature",
+            "properties": obj.get('attributes'),
+            "geometry": geom_parser(obj.get('geometry'))
+        }
 
     def get_descriptor_for_layer(self, layer):
-        return requests.get(self._build_request(layer), params={'f': 'pjson'}).json()
+        """
+        Returns the standard JSON descriptor for the layer.
+        """
+        if not self._layer_descriptor_cache.has_key(layer):
+            response = requests.get(self._build_request(layer), params={'f': 'pjson'})
+            self._layer_descriptor_cache[layer] = response.json()
+        return self._layer_descriptor_cache[layer]
 
     def enumerate_layer_fields(self, layer):
+        """
+        Pulls out all of the field names for a layer.
+        """
         descriptor = self.get_descriptor_for_layer(layer)
         return [field['name'] for field in descriptor['fields']]
 
-    def get(self, layer, where="1 = 1", fields=[], return_geometry=True):
+    def get(self, layer, where="1 = 1", fields=[], srid='4326'):
         """
         Gets a layer and returns it as honest to God GeoJSON.
 
@@ -105,21 +116,10 @@ class ArcGIS:
         # By default we grab all of the layer fields 
         fields = fields or self.enumerate_layer_fields(layer)
 
-        drv = ogr.GetDriverByName('KML')
-        kml = self.get_kml(layer, where, fields, return_geometry)
-        temp = tempfile.NamedTemporaryFile()
-        temp.write(kml)
-        temp.flush()
-
-        datasource = drv.Open(temp.name)
-        features = []
-        for layer in datasource:
-            for feat in layer:
-                jsobj = json.loads(feat.ExportToJson())
-                jsobj["properties"] = self._table_to_properties(jsobj)
-                features.append(jsobj)
+        jsobj = self.get_json(layer, where, fields)
+        geom_parser = self._determine_geom_parser(jsobj.get('geometryType'))
 
         return {
             'type': "FeatureCollection",
-            'features': features
+            'features': [self.esri_to_geojson(feat, geom_parser) for feat in jsobj.get('features')]
         }
