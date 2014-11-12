6027a68103ec372cc5c75833abf6ff2dc4203ad6
Ken Schwencke <schwank@gmail.com>
2014-11-12 12:09:35 -0500

    Updated for packaging

---
+++ setup.py
@@ -0,0 +1,23 @@
+import sys
+from setuptools import setup
+
+install_requires = [
+    "argparse==1.2.1",
+    "requests==2.4.3",
+    "wsgiref==0.1.2"
+]
+
+setup(
+    name='arcgis-rest-query',
+    version='0.0.2',
+    description='A tool to download a layer from an ArcGIS web service as GeoJSON',
+    author='Ken Schwencke',
+    author_email='schwank@gmail.com',
+    url='https://github.com/Schwanksta/python-arcgis-rest-query',
+    license='MIT',
+    packages=('arcgis',),
+    scripts=(
+        'bin/arcgis-get',
+    ),
+    install_requires=install_requires,
+)
